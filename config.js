c33936033becfcc690ac05d0473efc2a4c513f09
shravanipl <pl.shravani@gmail.com>
2018-10-31 17:29:57 -0500

    Fixed Test Mongo URL

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-module. exports = {
-    PORT : process.env.PORT || 8080,
-    HTTP_STATUS_CODES: {
-      OK : 200,
-      CREATED : 201,
-      NO_CONTENT : 204,
-      BAD_REQUEST : 400,
-      UNAUTHORISED : 401,
-      NOT_FOUND : 404,
-      INTERNAL_SERVER_ERROR : 500
-    },
-    MONGO_URL : process.env.MONGO_URL || 'mongodb://app:app123@ds113873.mlab.com:13873/money_mapper',
-    TEST_MONGO_URL: process.env.TEST_MONGO_URL || 'mongodb://localhost/money_mapper_test',
-    JWT_SECRET: process.env.JWT_SECRET || 'default',
-    JWT_EXPIRY : process.env.JWT_EXPIRY || '7d'
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  HTTP_STATUS_CODES: {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    UNAUTHORISED: 401,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500
+  },
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://app:app123@ds113873.mlab.com:13873/money_mapper',
+  TEST_MONGO_URL: process.env.TEST_MONGO_URL || 'mongodb://test@test1234@ds145043.mlab.com:45043/money_mapper_test',
+  JWT_SECRET: process.env.JWT_SECRET || 'default',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
